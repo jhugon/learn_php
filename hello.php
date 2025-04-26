fb9b3fd823da35d337b8f81260e24671752b4fce
Justin Hugon <jhugon@users.noreply.github.com>
2025-04-26 16:41:40 -0500

    messing with table constraints

--- hello.php
+++ hello.php
@@ -1,13 +1,11 @@
 <?php
-$txt = "PHP";
-echo "I love $txt!";
 
 $db = new PDO('sqlite::memory:');
 $db->exec("CREATE TABLE cars (
-make VARCHAR,
-model VARCHAR,
+make VARCHAR NOT NULL,
+model VARCHAR NOT NULL,
 year INT,
-color VARCHAR
+color VARCHAR NOT NULL
 );");
 
 $stmt = $db->prepare("INSERT INTO cars (make, model, year, color) VALUES (?,?,?,?)");
@@ -24,11 +22,17 @@ $year = 2010;
 $color = 'black';
 $stmt->execute([$make,$model,$year,$color]);
 
+$stmt = $db->prepare("INSERT INTO cars (make, model, color) VALUES (?,?,?)");
+
+$make = 'Ford';
+$model = 'T';
+$color = 'black';
+$stmt->execute([$make,$model,$color]);
+
 $query = $db->query("SELECT * FROM cars");
 $query->setFetchMode(PDO::FETCH_ASSOC);
 // $result = $query->fetchAll();
 foreach ($query as $row) {
-    print_r($row);
     echo "Row:\n";
     foreach ($row as $key => $value) {
         echo "$key: $value\n";
